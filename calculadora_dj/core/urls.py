e11cbc18c6da6e279a3a73a915646e1902340393
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2023-11-24 18:27:34 +0100

    django

--- calculadora_dj/core/urls.py
+++ calculadora_dj/core/urls.py
@@ -18,8 +18,5 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('admin/', admin.site.urls),
-
+    path('', views.home, name="home")
 ]
